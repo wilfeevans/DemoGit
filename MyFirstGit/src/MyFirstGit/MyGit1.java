b78a0e099cb2d6747945fa7b71ff2195820e5c86
wilfred <wilfeevans@gmail.com>
2021-01-06 20:23:24 +0530

    committing MyGit1.java

---
+++ MyFirstGit/src/MyFirstGit/MyGit1.java
@@ -0,0 +1,16 @@
+package MyFirstGit;
+
+public class MyGit1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int a=101;
+		int b=70;
+		int sum= a + b;
+		System.out.println("Addition:" + sum);
+				
+
+	}
+
+}
