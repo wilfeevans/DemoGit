f0ba4bb445ce33df049dda2201c9ed854b663873
wilfred <wilfeevans@gmail.com>
2021-01-07 11:14:14 +0530

    My Git2.java created and committed

---
+++ MyFirstGit/src/MyFirstGit/MyGit2.java
@@ -0,0 +1,16 @@
+package MyFirstGit;
+
+public class MyGit2 {
+
+	public static void main(String[] args) 
+	{
+	
+		int a=101;
+		int b=70;
+		int subtract= a - b;
+		System.out.println("Subtract:" + subtract);
+				
+
+	}
+
+}
